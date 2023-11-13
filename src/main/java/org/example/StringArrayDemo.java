c4022905b18cd06ec5f942357db3046efae6cbd1
mahadev kallolli <mahadevk282@gmail.com>
2023-11-13 20:02:01 +0530

    GenericsFirstDemo

---
+++ src/main/java/org/example/StringArrayDemo.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class StringArrayDemo {
+    public static void main(String[] args) {
+        String[] s= new String[100];
+        s[0]= "Rahul";
+        s[1]= "Ram";
+        s[2]= "vijay";
+        s[3]= "Rocky";
+        s[4]= "rose";
+        //s[5]= new Integer(10);
+        System.out.println("0."+s[0]+"\n1."+s[1]);
+
+    }
+}
