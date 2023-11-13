0c8a187bf8574a142fb031a8d617df0bc5a20abb
mahadev kallolli <mahadevk282@gmail.com>
2023-11-13 20:21:29 +0530

    "StringArrayListDemo"

--- src/main/java/org/example/StringArrayListDemo.java
+++ src/main/java/org/example/StringArrayListDemo.java
@@ -1,5 +1,20 @@
 package org.example;
 
-public class StringArrayListDemo {
+import java.util.ArrayList;
 
+public class StringArrayListDemo {
+    public static void main(String[] args) {
+        //without generics
+        ArrayList l = new ArrayList();
+        l.add("rahul");
+        l.add("rocky");
+        l.add(1002);
+        String name1=(String) l.get(0);
+        String name2=(String) l.get(1);
+        String name3=(String) l.get(2);
+        System.out.println(name1+"\n"+name2);
+    }
 }
+/*Exception in thread "main" java.lang.ClassCastException: class java.lang.Integer cannot be cast to class
+java.lang.String (java.lang.Integer and java.lang.String are in module java.base of loader 'bootstrap')
+ at org.example.StringArrayListDemo.main(StringArrayListDemo.java:14)*/
