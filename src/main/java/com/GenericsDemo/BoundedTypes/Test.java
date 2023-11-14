d53c105a3791fc5f7b451428fde2651301b6b4b4
mahadev kallolli <mahadevk282@gmail.com>
2023-11-14 19:57:52 +0530

    "without bounded types"

---
+++ src/main/java/com/GenericsDemo/BoundedTypes/Test.java
@@ -0,0 +1,9 @@
+package com.GenericsDemo.BoundedTypes;
+//without bounded types
+public class Test<T> {
+    public static void main(String[] args) {
+        Test<Integer> t1= new Test<Integer>();
+        Test<String> t2= new Test<String>();
+    }
+
+}
