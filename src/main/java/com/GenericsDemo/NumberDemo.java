600c40d4fbfb8c80fb47df877ec506589f00cd78
mahadev kallolli <mahadevk282@gmail.com>
2023-11-14 19:34:18 +0530

    "Number x = 100 generic class also"

---
+++ src/main/java/com/GenericsDemo/NumberDemo.java
@@ -0,0 +1,9 @@
+package com.GenericsDemo;
+
+public class NumberDemo {
+    public static void main(String[] args) {
+       Number x = 100;
+        System.out.println(x);
+        System.out.println(x.intValue());
+    }
+}
